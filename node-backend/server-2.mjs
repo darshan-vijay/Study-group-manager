951a00ff0fd7b8ac9dcde60115e541d5046d09d4
darshan-vijay <vdarshan4@gmail.com>
2024-11-19 00:20:58 -0700

    rabbitMQ communication created

---
+++ node-backend/server-2.mjs
@@ -0,0 +1,32 @@
+import amqplib from "amqplib";
+
+const RABBITMQ_URL = 'amqp://localhost'; // Replace with your RabbitMQ URL
+const QUEUE_NAME = 'jsonMessages';
+
+async function consumeMessages() {
+    const connection = await amqplib.connect(RABBITMQ_URL);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(QUEUE_NAME, { durable: true });
+
+    console.log('Waiting for messages...');
+
+    channel.consume(
+        QUEUE_NAME,
+        (message) => {
+            if (message) {
+                const parsedMessage = JSON.parse(message.content.toString());
+                console.log('Received message:', parsedMessage);
+
+                // Acknowledge the message
+                channel.ack(message);
+            }
+        },
+        { noAck: false }
+    );
+}
+
+    consumeMessages().catch((error) => {
+      console.error('Error in consumer:', error);
+    });
+
